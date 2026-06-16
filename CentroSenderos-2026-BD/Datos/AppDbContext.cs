26a28284dbea096edfbedf1539d50456083c80e5
JoaquinRiveroAlvarez <joaquinrivero0707@gmail.com>
2026-06-16 10:24:49 -0300

    Cambios

--- CentroSenderos-2026-BD/Datos/AppDbContext.cs
+++ CentroSenderos-2026-BD/Datos/AppDbContext.cs
@@ -1,18 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using CentroSenderos_2026_BD.Datos;
-//using CentroSenderos_2026_BD.Datos.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CentroSenderos_2026_BD.Datos.Entity;
 
 namespace CentroSenderos_2026_BD
 {
     public class ApplicationDbContext : IdentityDbContext<MiUsuario>
     {
-        //public DbSet <TipoProducto> TipoProductos { get; set; }
+        public DbSet <Profesional> Profesionales { get; set; }
+        public DbSet<Profesional> TipoProductos { get; set; }
 
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
