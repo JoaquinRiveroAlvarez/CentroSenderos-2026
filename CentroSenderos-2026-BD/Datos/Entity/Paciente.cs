fb0989db4d2fd0f8f4ccf813ec058917782ca449
JoaquinRiveroAlvarez <joaquinrivero0707@gmail.com>
2026-06-18 20:23:45 -0300

    Revert "Repo, controller - paciente"
    This reverts commit ac91ad7f19f0d585911b99c7a1cd18915d70b4b2.

--- CentroSenderos-2026-BD/Datos/Entity/Paciente.cs
+++ CentroSenderos-2026-BD/Datos/Entity/Paciente.cs
@@ -1,5 +1,4 @@
-﻿using CentroSenderos_2026_Shared.Enum;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -8,7 +7,6 @@ using System.Text;
 namespace CentroSenderos_2026_BD.Datos.Entity
 {
     [Index(nameof(DNI), Name = "DNI_UQ", IsUnique = true)]
-    [Index(nameof(HistoriaClinica), Name = "HistoriaClinica_UQ", IsUnique = true)]
     public class Paciente : EntityBase
     {
         [Required(ErrorMessage = "El Nombre es obligatorio")]
@@ -19,19 +17,12 @@ namespace CentroSenderos_2026_BD.Datos.Entity
         [MaxLength(10, ErrorMessage ="Maxima cant. caracteres 10")]
         public required string DNI { get; set; }
 
-        [Required(ErrorMessage = "El Género es obligatorio")]
-        public required EnumSeleccionarGenero Genero { get; set; }
-
         [Required(ErrorMessage ="La Fecha de Nacimiento es obligatoria")]
         public required DateTime FechaNacimiento { get; set; }
 
         [Required(ErrorMessage = "El NroAfiliado es obligatorio")]
         [MaxLength(30, ErrorMessage = "Maxima cant. caracteres 30")]
-        public required int NumeroAfiliado { get; set; }
-
-        [Required(ErrorMessage = "La Historia Clinica es obligatoria")]
-        [MaxLength(30, ErrorMessage = "Maxima cant. caracteres 30")]
-        public required int HistoriaClinica { get; set; }
+        public required int NroAfiliado { get; set; }
 
         [Required(ErrorMessage = "El Telefono es obligatorio")]
         [MaxLength(30, ErrorMessage = "Maxima cant. caracteres 30")]
@@ -41,19 +32,11 @@ namespace CentroSenderos_2026_BD.Datos.Entity
         [MaxLength(30, ErrorMessage = "Maxima cant. caracteres 30")]
         public required string Domicilio { get; set; }
 
-        [Required(ErrorMessage = "El Correo Electrónico es obligatorio")]
-        public required string CorreoElectronico { get; set; }
-
-
-
-        public int ProfesionalId { get; set; }
-        public Profesional? Profesionales { get; set; }
         public int TipoObraSocialId { get; set; }
         public TipoObraSocial? TipoObraSociales { get; set; }
+
         public int TipoDiagnosticoId { get; set; }
         public TipoDiagnostico? TipoDiagnosticos { get; set; }
-        public int TipoDocumentoId { get; set; }
-        public TipoDocumento? TipoDocumentos { get; set; }
         
     }
 }
