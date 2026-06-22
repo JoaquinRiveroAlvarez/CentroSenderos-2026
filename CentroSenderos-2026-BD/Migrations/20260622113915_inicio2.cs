b279816f3ed9af948a2dfb2637c650a79dcc98d5
JoaquinRiveroAlvarez <joaquinrivero0707@gmail.com>
2026-06-22 08:40:51 -0300

    udate-database/nueva migration

---
+++ CentroSenderos-2026-BD/Migrations/20260622113915_inicio2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CentroSenderos_2026_BD.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicio2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nombre",
+                table: "TipoObrasSociales");
+
+            migrationBuilder.DropColumn(
+                name: "FechaNacimiento",
+                table: "Pacientes");
+
+            migrationBuilder.RenameColumn(
+                name: "NroAfiliado",
+                table: "Pacientes",
+                newName: "NumeroAfiliado");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefono",
+                table: "Pacientes",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 30);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NumeroAfiliado",
+                table: "Pacientes",
+                newName: "NroAfiliado");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nombre",
+                table: "TipoObrasSociales",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Telefono",
+                table: "Pacientes",
+                type: "int",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaNacimiento",
+                table: "Pacientes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
