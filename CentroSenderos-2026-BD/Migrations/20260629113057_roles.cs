7601ee802c167e64677864d90a21582a6dd6e9d1
JoaquinRiveroAlvarez <joaquinrivero0707@gmail.com>
2026-06-30 08:37:39 -0300

    Datos y Roles

---
+++ CentroSenderos-2026-BD/Migrations/20260629113057_roles.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CentroSenderos_2026_BD.Migrations
+{
+    /// <inheritdoc />
+    public partial class roles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var clave = Guid.NewGuid().ToString();
+            var nombreRol = "admin";
+            var nombreRolNormalizado = nombreRol.ToUpper();
+
+            migrationBuilder.Sql(
+                "INSERT INTO \"AspNetRoles\" (\"Id\", \"Name\", \"NormalizedName\") VALUES ('"
+                + clave + "', '" + nombreRol + "', '" + nombreRolNormalizado + "')"
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM \"AspNetRoles\" WHERE \"Name\" = 'admin'");
+        }
+    }
+}
