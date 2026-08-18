40be80677c2dd11d3e1df68be0fecb35b93ce522
Zoe <mzoemoroncini@hotmail.com>
2026-08-18 14:43:09 -0300

    mensaje de telefono mas pro

--- CentroSenderos-2026-Shared/DTO/ProfesionalDTO.cs
+++ CentroSenderos-2026-Shared/DTO/ProfesionalDTO.cs
@@ -9,51 +9,34 @@ namespace CentroSenderos_2026_Shared.DTO
 
 
         [Required(ErrorMessage = "Ingresá el nombre completo.")]
-        [MaxLength(
-            100,
-            ErrorMessage = "El nombre no puede superar los 100 caracteres."
-        )]
+        [MaxLength(100,ErrorMessage = "El nombre no puede superar los 100 caracteres.")]
         public required string Nombre { get; set; } = string.Empty;
 
 
         [Required(ErrorMessage = "Ingresá el área profesional.")]
-        [MaxLength(
-            100,
-            ErrorMessage = "El área no puede superar los 100 caracteres."
-        )]
+        [MaxLength(100,ErrorMessage = "El área no puede superar los 100 caracteres.")]
         public required string Area { get; set; } = string.Empty;
 
 
         [Required(ErrorMessage = "Ingresá el CUIT.")]
         [MaxLength(30, ErrorMessage = "El CUIT no puede superar los 30 caracteres.")]
-        [RegularExpression(
-     @"^\d{2}-\d{8}-\d{1}$",
-     ErrorMessage = "El CUIT debe tener el formato XX-XXXXXXXX-X."
- )]
+        [RegularExpression(@"^\d{2}-\d{8}-\d{1}$",ErrorMessage = "El CUIT debe tener el formato XX-XXXXXXXX-X.")]
         public required string Cuit { get; set; } = string.Empty;
 
 
         [Required(ErrorMessage = "Ingresá la Matrícula Profesional.")]
-        [MaxLength(
-            30,
-            ErrorMessage = "La Matrícula Profesional no puede superar los 30 caracteres."
-        )]
+        [MaxLength(30,ErrorMessage = "La Matrícula Profesional no puede superar los 30 caracteres.")]
         public required string MP { get; set; } = string.Empty;
 
 
         [Required(ErrorMessage = "Ingresá el RNP.")]
-        [MaxLength(
-            30,
-            ErrorMessage = "El RNP no puede superar los 30 caracteres."
-        )]
+        [MaxLength(30,ErrorMessage = "El RNP no puede superar los 30 caracteres.")]
         public required string RNP { get; set; } = string.Empty;
 
 
         [Required(ErrorMessage = "Ingresá el teléfono.")]
-        [MaxLength(
-            30,
-            ErrorMessage = "El teléfono no puede superar los 30 caracteres."
-        )]
+        [MaxLength(30,ErrorMessage = "El teléfono no puede superar los 30 caracteres.")]
+        [RegularExpression(@"^[0-9+\-\s()]+$", ErrorMessage = "El teléfono solo puede contener números, espacios, +, -, y paréntesis.")]
         public required string Telefono { get; set; } = string.Empty;
 
 
