aa1773353c8a547750c6404ba5b3c1f7dcaac74e
JoaquinRiveroAlvarez <joaquinrivero0707@gmail.com>
2026-09-06 12:10:29 -0300

    RolCalendario

---
+++ CentroSenderos-2026-BD/Migrations/20260901114553_RolCalendario.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CentroSenderos_2026_BD.Migrations
+{
+    /// <inheritdoc />
+    public partial class RolCalendario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            InsertarRol(migrationBuilder, "Calendario");
+        }
+
+        private void InsertarRol(MigrationBuilder migrationBuilder, string nombreRol)
+        {
+            var clave = Guid.NewGuid().ToString();
+            var nombreRolNormalizado = nombreRol.ToUpper();
+
+            migrationBuilder.Sql(
+                "INSERT INTO \"AspNetRoles\" (\"Id\", \"Name\", \"NormalizedName\") VALUES ('"
+                + clave + "', '" + nombreRol + "', '" + nombreRolNormalizado + "')"
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM \"AspNetRoles\" WHERE \"Name\" IN ('Calendario')");
+        }
+    }
+}
