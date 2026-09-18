3ea302e4faa5155780bac8e8a663edae1883e523
Zoe <mzoemoroncini@hotmail.com>
2026-09-18 19:57:51 -0300

    Corrige columna DocumentoId faltante en Pacientes

---
+++ CentroSenderos-2026-BD/Migrations/20260918222539_AgregarDocumentoIdPaciente.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CentroSenderos_2026_BD.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarDocumentoIdPaciente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DocumentoId",
+                table: "Pacientes",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pacientes_DocumentoId",
+                table: "Pacientes",
+                column: "DocumentoId");
+
+            //migrationBuilder.AddForeignKey(
+            //    name: "FK_Pacientes_Documento_DocumentoId",
+            //    table: "Pacientes",
+            //    column: "DocumentoId",
+            //    principalTable: "Documento",
+            //    principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.DropForeignKey(
+            //    name: "FK_Pacientes_Documento_DocumentoId",
+            //    table: "Pacientes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pacientes_DocumentoId",
+                table: "Pacientes");
+
+            migrationBuilder.DropColumn(
+                name: "DocumentoId",
+                table: "Pacientes");
+        }
+    }
+}
